937830912632bef748aa5e5696a827584b948ad2
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-16 22:47:16 -0700

    hii

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class2
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("This is Me Method1");
+        }
+    }
+    public class Class3 : Class2
+    {
+        public override void display()
+        {
+            Console.WriteLine("This is Method2");
+        }
+        public static void Main(String[] args)
+        {
+            Class3 ob = new Class3();
+            ob.display();
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        Program()
+        {
+            Console.WriteLine("This is me Constructor");
+        }
+       
+        static void Main(string[] args)
+        {
+           new Program();
+        }
+    }
+}
