b50e6465678c785370c2079a2be63e03b344bd7c
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-17 02:13:22 -0700

    Hey

---
+++ ConsoleApp3/ConsoleApp3/Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Method
+    {
+        public void Display(int a , int b) {
+            int c = a + b;
+            Console.WriteLine("The value of c is : " + c);
+        }
+        public void Display(double a , double b)
+        {
+            double c = a + b;
+            Console.WriteLine("The value of c is : "+c);
+        }
+        public void Display(int a , double b)
+        {
+            double c = a + b;
+            Console.WriteLine("The value of c is : " + c);
+        }
+        public void Display(float a , float b)
+        {
+            double d = a + b;
+            Console.WriteLine("The value of c is : " +d);
+        }
+        public void Display(string c , string d)
+        {
+            Console.WriteLine(string.Concat(c, d));
+        }
+
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/temp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class temp
+    {
+        public int a;
+        public int b;
+        public static temp operator +(temp fir, temp sec)
+        {
+            temp third = new temp();
+            third.a=fir.a+sec.a;
+            third.b=fir.b+sec.b;
+            return third;
+        }
+        public static void Main(string[] args)
+        {
+           temp t1= new temp();
+            t1.a = 10;t1.b = 20;
+            temp t2= new temp();
+            t2.a = 30; t2.b = 40;
+            temp t3 = t1 + t2;
+            Console.WriteLine(t3.a + " " + t3.b);
+        }
+    }
+   
+}
