36150e6eafcf9485fd41be153ea5ab6dd4d64e80
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-16 23:19:51 -0700

    this Is Abhay

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2   //This is Program of Inheritance//
+{
+    public class Program
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("this is display1");
+        }
+    }
+        public class Program2 : Program
+        {     
+           public override void display() {
+            Console.WriteLine("this is display2");
+        }
+      static void Main(string[] args)
+        {
+            Program2 obj = new Program2();
+            obj.display();
+        }
+    }
+}
