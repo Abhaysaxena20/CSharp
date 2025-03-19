e739e824c8dbda5647db7fa5749a1a2282f2bf21
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-19 12:30:43 -0700

    This is static class

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public static class Program
+    {
+        public static string Name;
+        public static string DOB;
+        public static double salary;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Name is :Abhay " + Program.Name);
+            Console.WriteLine("DOB is : 16/04/2004" + Program.DOB);
+            Console.WriteLine("salary is :35000" + Program.salary);
+        }
+    }
+}
