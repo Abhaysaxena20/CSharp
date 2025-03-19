da7e5ec7d8f60c59363f610d72036bf23d6f4d8e
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-19 12:14:35 -0700

    This a consoleAPpp5

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        enum books
+        {
+            MATHS,PHYSICS,CHEMISTRY,BIOLOGY,COMMERCE
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maths book is : "+books.MATHS);
+            Console.WriteLine("Physics book is : "+books.PHYSICS);
+            Console.WriteLine("Biology book is : "+books.BIOLOGY);
+            Console.WriteLine("Commerce book is : "+books.COMMERCE);
+            Console.WriteLine("Chemistry book is : "+books.CHEMISTRY);
+        }
+    }
+}
