64d10b34f2d14dbe4434e1f60af3e53fc8d4da26
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-19 12:12:07 -0700

    This is a file

---
+++ ConsoleApp4/ConsoleApp4/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Class1
+    {
+        public string Name;
+        public string EMAIL;
+        public Class1(string name,string email)
+        {
+            this.Name = name;
+            this.EMAIL = email;
+        }
+        public Class1(Class1 obj)
+        {
+            this.Name = obj.Name;
+            this.EMAIL = obj.EMAIL;
+
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj2 = new Class1("Anubhav", "anubhav123@gmail.com");
+            Class1 obj3 = new Class1(obj2);
+            Console.WriteLine(obj3.Name+" "+obj3.EMAIL+" ");
+        }
+    }
+}
