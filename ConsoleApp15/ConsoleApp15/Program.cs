71e1b3f442c0adb7ffcfe9d880f964300fd20c86
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-21 02:47:10 -0700

    20/03/2025

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string path = @"D:\GIT\CSharp";
+            var Directory =new DirectoryInfo(path);
+            Console.WriteLine("Directory Name = "+Directory.FullName);
+            Console.WriteLine("Directory Size = "+Directory.Parent);
+            Console.WriteLine("Directory Create Date = "+Directory.CreationTime);
+            Console.WriteLine("Directory Last Access Date = "+Directory.LastAccessTime);
+
+        }
+           
+    }
+}
