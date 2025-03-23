f669f80d64f80317b0ffd7cb35889d7a086b7ac1
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-23 12:42:09 -0700

    23/03/2025

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drive = DriveInfo.GetDrives();
+            foreach (DriveInfo dr2 in drive)
+            {
+                Console.WriteLine("======================Drive Name======================");
+                Console.WriteLine("Drive FullName : " + dr2.Name);
+                Console.WriteLine("Drive Size : " + dr2.TotalSize);
+                Console.WriteLine("Drive Freespace: " + dr2.TotalFreeSpace);
+                Console.WriteLine("Drive Format : " + dr2.DriveFormat);
+                Console.WriteLine("Drive Type : " + dr2.GetType().Name);
+            }
+        }
+    }
+}
