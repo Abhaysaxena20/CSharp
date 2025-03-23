a15e9b81927fc30fe14846421dd61e87cfb358a0
Abhaysaxena20 <abhaysaxena016@gmail.com>
2025-03-23 12:43:26 -0700

    23/03/2025

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 2, 7, 8, 12, 21, 9, 54, 23 ,1,2,3};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
